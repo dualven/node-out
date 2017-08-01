ff9669e1d5ba969b3a5c8e6e6e8f012eaf78221e
dualven <dualven@163.com>
2017-08-01 16:04:26 +0800

    2017-7-29 modify

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -75,42 +75,78 @@ router.post('/index/out', function(req, res, next) {
 router.get('/index/welcome', function(req, res, next) {
     res.render('index/welcome');
 });
-router.get('/', function(req, res, next) {
-    var data = new Array();
-    var _data1 = Array({
-        'name':'导出',
-        'mca':'index/out',
-        'fa_home':'nan'
-    });
-    var _data2 = Array({
-        'name':'导入',
-        'mca':'index/in',
-        'fa_home':'nan22'
-    });
-     var _data3 = Array({
-        'name':'拓扑',
-        'mca':'index/topo',
-        'fa_home':'nan33'
-    });
-    data.push({
-        'name': '导出父菜单',
-        'fa_home':'nan',
-        '_data':_data1
-    });
-    data.push({
-        'name': '导入父菜单',
-        'fa_home':'nan2',
-        '_data':_data2
-    });
-     data.push({
-        'name': '拓扑父菜单',
-        'fa_home':'nan3',
-        '_data':_data3
-    });
+//router.get('/', function(req, res, next) {
+//    var data = new Array();
+//    var _data1 = Array({
+//        'name':'导出',
+//        'mca':'index/out',
+//        'fa_home':'nan'
+//    });
+//    var _data2 = Array({
+//        'name':'导入',
+//        'mca':'index/in',
+//        'fa_home':'nan22'
+//    });
+//     var _data3 = Array({
+//        'name':'拓扑',
+//        'mca':'index/topo',
+//        'fa_home':'nan33'
+//    });
+//    data.push({
+//        'name': '导出父菜单',
+//        'fa_home':'nan',
+//        '_data':_data1
+//    });
+//    data.push({
+//        'name': '导入父菜单',
+//        'fa_home':'nan2',
+//        '_data':_data2
+//    });
+//     data.push({
+//        'name': '拓扑父菜单',
+//        'fa_home':'nan3',
+//        '_data':_data3
+//    });
+//    res.render('index', {
+//        username: 'duanxiongwen',
+//        usergroup: 'dxw' ,
+//        data: data
+//    });
+//});
+function mongo(){
+var MongoClient = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://localhost:27017/dualven';  
+
+var selectData = function(db, callback) {  
+  //连接到表  
+  var collection = db.collection('fav');
+  //查询数据
+  var whereStr = {};
+  collection.find(whereStr).toArray(function(err, result) {
+    if(err)
+    {
+      console.log('Error:'+ err);
+      return;
+    }     
+    callback(result);
+  });
+}
+
+MongoClient.connect(DB_CONN_STR, function(err, db) {
+  console.log("连接成功！");
+  selectData(db, function(result) {
+    console.log(JSON.stringify(result));
+    router.get('/', function(req, res, next) {
+   
     res.render('index', {
         username: 'duanxiongwen',
         usergroup: 'dxw' ,
-        data: data
+        data: result
     });
 });
+    db.close();
+  });
+});
+}
+mongo();
 module.exports = router;
