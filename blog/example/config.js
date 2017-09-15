95b2706a157558efb10299a548f96abba2a74aab
dualven <dualven@163.com>
2017-09-15 13:02:38 +0800

    config

--- blog/example/config.js
+++ blog/example/config.js
@@ -1,40 +1,51 @@
 module.exports = {
-  '/login/db': {
-    'GET': ['customer'],
-  },
-//
-  '/index/welcome': {
-    '*': true,
-  },
-//
-//  '/api/users/:id/name': {
-//    '*': ['admin'],
-//    'GET': ['user', 'admin'],
-//    'POST': ['admin'],
-//  },
-//
-//  '/api/users': {
-//    '*': ['admin'],
-//    'GET': ['user'],
-//    'POST': ['user'],
-//  },
-//
-//  '/api/user-only': {
-//    '*': ['user'],
-//  },
-//
-//  '/api/admin-only': {
-//    '*': ['admin'], 
-//  },
-  '/login': {
-    'GET': true, 
-    'POST': true,
-  },
+    '/login': {
+        'GET': true,
+        'POST': true,
+    },
+    '/login/db': {
+        'GET': ['customer'],
+    },
+    '/login/dbinfo': {
+        'POST': ['customer'],
+    },
+    '/login/getdbs': {
+        'POST': ['customer'],
+    },
+    '/login/gettables': {
+        'POST': ['customer'],
+    },
+    '/login/getcolumns': {
+        'POST': ['customer'],
+    },
+     '/login/acceptfile': {
+        'POST': ['customer'],
+    },
+     '/login/output': {
+        'POST': ['customer'],
+    },
+     '/login/inputxlsx': {
+        'POST': ['customer'],
+    },
     '/login/check': {
-    'GET': true, 
-    'POST':true,
-  },
-  '/': {
-    'GET': ['admin','customer'], 
-  },
+        'GET': true,
+        'POST': true,
+    },
+    '/': {
+        'GET': ['admin', 'customer', null],
+    },
+       '/index/out': {
+        'GET': ['customer'],
+    },   '/index/in': {
+        'GET': ['customer'],
+    },
+       '/index/welcome': {
+       'GET': ['customer'],
+    },
+       '/index/profile': {
+        'GET': ['customer'],
+    },
+      '/index/topo': {
+      'GET': ['customer'],
+    },
 }
