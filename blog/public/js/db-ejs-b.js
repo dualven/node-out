b785f3d2c2b2d05924a705b18f36ea41c24066b0
dualven <dualven@163.com>
2017-11-07 10:51:05 +0800

    add excel export but style is not right

--- blog/public/js/db-ejs-b.js
+++ blog/public/js/db-ejs-b.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     var oTable1 = $("#onlinetable").on('xhr.dt', function (e, settings, json, xhr) {
         for (var i = 0, ien = json.data.length; i < ien; i++) {
             json.data[i].customer_name = json.data[i].customer.customer_name;
+            json.data[i].onlinetime = new Date(json.data[i].onlinetime * 1000).toLocaleString();
         }
         // Note no return - manipulate the data directly in the JSON object.
     }).dataTable({
@@ -16,7 +17,7 @@ $(document).ready(function () {
             {"data": "street"},
             {"data": "onlinetime"},
             {"data": "customer_id"},
-            {"data": "customer_name"}, 
+            {"data": "customer_name"},
             {"data": "created_at_time"}
         ],
 
@@ -24,7 +25,7 @@ $(document).ready(function () {
     var oTable2 = $("#offlinetable").on('xhr.dt', function (e, settings, json, xhr) {
         for (var i = 0, ien = json.data.length; i < ien; i++) {
             json.data[i].customer_name = json.data[i].customer.customer_name;
-            json.data[i].offlinetime= new Date(json.data[i].offlinetime * 1000).toLocaleString();
+            json.data[i].offlinetime = new Date(json.data[i].offlinetime * 1000).toLocaleString();
 //            var unixTimestamp = new Date(Unix timestamp * 1000) 然后 commonTime = unixTimestamp.toLocaleString()
         }
         // Note no return - manipulate the data directly in the JSON object.
@@ -40,6 +41,15 @@ $(document).ready(function () {
             {"data": "customer_id"},
             {"data": "customer_name"},
             {"data": "created_at"}
-        ]
+        ],
+         dom: 'Bfrtip',
+        buttons: [ {
+            extend: 'excelHtml5',
+            customize: function( xlsx ) {
+                var sheet = xlsx.xl.worksheets['sheet1.xml'];
+ 
+                $('row c[r^="C"]', sheet).attr( 's', '2' );
+            }
+        } ]
     });
 });
